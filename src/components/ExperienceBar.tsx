ac0b2f6df62bc8d67b20c4334e07dc1d5f342a08
LuanEleuterio <luaneleuterio3@hotmail.com>
2021-02-24 21:11:38 -0300

    Finalizado a contagem do countdown. Feito o component de challenges e inserido o Context API para transportar os dados dos challenges para os demais components

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,21 +1,26 @@
+import { useContext } from 'react';
 import { BsStar } from 'react-icons/bs';
 import { BsStarHalf } from 'react-icons/bs';
 import { BsStarFill } from 'react-icons/bs';
 import { IoIosRocket } from 'react-icons/io';
+import { ChallengesContext } from '../context/ChallengesContext';
 
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext)
+
+  const progressToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
 
   return (
     <header className={styles.experienceBar}>
       <span><BsStar /> 0 exp </span>
       <div>
-        <div style={{ width: '60%' }} />
-        <IoIosRocket className={styles.svgRocket} style={{ left: '60%' }} />
-        <span className={styles.currentExperience} style={{ left: '60%' }}><BsStarHalf /> 400px</span>
+        <div style={{ width: `${progressToNextLevel}%` }} />
+        <IoIosRocket className={styles.svgRocket} style={{ left: `${progressToNextLevel}%` }} />
+        <span className={styles.currentExperience} style={{ left: `${progressToNextLevel}%` }}><BsStarHalf /> {currentExperience} exp</span>
       </div>
-      <span><BsStarFill /> 600 exp</span>
+      <span><BsStarFill /> {experienceToNextLevel} exp</span>
     </header>
   )
 }
